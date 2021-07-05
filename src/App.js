9de706a68e3c47d80d92e9810441f33dcae49104
André <andreglock@gmail.com>
2021-07-05 14:53:55 +0200

    done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>Hello React!</header>
+      <sidebar>Sidebar</sidebar>
+      <main>lorem ipsum</main>
+      <footer>Made by a highly trained not a monkey</footer>
+    </div>
+  );
+}
+
+export default App;
